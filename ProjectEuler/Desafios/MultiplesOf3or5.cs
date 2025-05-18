7051e5c74160e9b794dfb542f42593e112525925
devguilherme-b <hitsnoplaymusic@gmail.com>
2025-05-18 17:49:17 -0300

    Adding project with first solution in CSharp

---
+++ ProjectEuler/Desafios/MultiplesOf3or5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ProjectEuler.Desafios;
+
+namespace ProjectEuler.Desafios;
+
+public class MultiplesOf3or5
+{
+
+
+    public static void SearchMultiplesOf3or5()
+    {
+        // HashSet<T> para armazenar os multiplos 
+        HashSet<int> multiples = new HashSet<int>();
+        // Valores de entrad
+        int valorA = 3;
+        int valorB = 5;
+        // Valor para a condicional
+        int condicao = 1000;
+        int resto;
+        for (int y = 1; y < condicao; y++)
+        {
+            resto = y % valorA;
+            if (resto == 0)
+                multiples.Add(y);
+        }
+        for (int z = 1; z < condicao; z++)
+        {
+            resto = z % valorB;
+            if (resto == 0)
+                multiples.Add(z);
+        }
+        int soma = multiples.Sum();
+        Console.WriteLine($"O resultado é: {soma}");
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using ProjectEuler.Desafios;
+
+namespace ProjectEuler;
+
+public static class Program
+{
+    public static void Main()
+    {
+        // 1° Problem : Multiples of 3 or 5 between 0 and 1000
+        MultiplesOf3or5.SearchMultiplesOf3or5();
+
+        // 2° Problem : Numbers Fibonacci
+        // In development
+    }
+}
